3663d251bba327bd1149d6342517327a47dc37eb
evx001 <eviexeris@gmail.com>
2014-01-20 20:08:41 -0500

    WadlerOnLists

---
+++ Ch01/ch1Lists.hs
@@ -0,0 +1,13 @@
+module Ch1Lists where 
+-- Lists are the primary data structure in LISP like langauges.
+-- Where as Arrays are the primary data collection in C derived languages like Java.
+-- [x*x | x <- [1,2,3] ] 
+
+-- [toLower c | c <- "Hello From Haskell"] 
+
+-- [ (x, even x) | x <- [1,2,3] ] 
+
+-- x <- [1,2,3] is a generator 
+-- <- means "drawn from" 
+
+ 
