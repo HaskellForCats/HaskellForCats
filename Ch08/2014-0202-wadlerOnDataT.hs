dc6d5d915ecac5dd6cafa68919f9fb99ba8165fe
evx001 <eviexeris@gmail.com>
2014-02-02 06:24:21 -0500

    all  the types

--- Ch08/2014-0202-wadlerOnDataT.hs
+++ Ch08/2014-0202-wadlerOnDataT.hs
@@ -1,3 +1,14 @@
 module Feb02 where 
 
+data Bool = False | True 
+data Season = Winter | Spring | Summer | Fall 
+data Shape = Circle Float | Rectangle | Float 
+data Exp = List Int | Add Exp Exp | Mul Exp Exp 
+data List a = Nil | Cons a (List a) 
+data Nat = Zero | Succ Nat 
+data Tree a = Empty | Leaf a | Branch | (Tree a) (Tree a) 
+data Maybe a = Nothing | Just a 
+data Pair a b = Pair a b 
+data Sum ab = Left a | Right b 
+ 
 
