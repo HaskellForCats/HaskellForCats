ac27e19d83a58000464e45797e86f9fe7343dd26
evx001 <eviexeris@gmail.com>
2014-02-02 07:02:47 -0500

    Bool

--- Ch08/2014-0202-wadlerOnDataT.hs
+++ Ch08/2014-0202-wadlerOnDataT.hs
@@ -1,5 +1,19 @@
 module Feb02 where 
 
+data Bool = False | True
+-- pattern match the values 
+nott False = True 
+nott True  = False 
+
+-- note that True is Identity for AND
+False   `&_&` q = False 
+True    `&_$` q = q 
+
+-- note False is Identity here for OR
+False   `|_|` q = q 
+True    `|_|` q = True 
+
+{-
 data Bool = False | True 
 data Season = Winter | Spring | Summer | Fall 
 data Shape = Circle Float | Rectangle | Float 
@@ -10,5 +24,5 @@ data Tree a = Empty | Leaf a | Branch | (Tree a) (Tree a)
 data Maybe a = Nothing | Just a 
 data Pair a b = Pair a b 
 data Sum ab = Left a | Right b 
- 
+-}
 
