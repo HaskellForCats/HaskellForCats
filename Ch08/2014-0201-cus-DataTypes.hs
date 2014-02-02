6280632a484250b1cd1aeeca4205dc547aedee71
evx001 <eviexeris@gmail.com>
2014-02-01 22:34:26 -0500

    Screen Shoot Me

--- Ch08/2014-0201-cus-DataTypes.hs
+++ Ch08/2014-0201-cus-DataTypes.hs
@@ -1,19 +1,19 @@
-module CusDataTyp where 
+         module CusDataTyp where 
 -- : set expandtab ts=2 ruler number spell
 
 -- 47 days of Haskell 
 
 -- TYPE SYNONYMS 
 
-type MyStrings = [Char] 
-type Point = (Double,Double)
+         type MyStrings = [Char] 
+         type Point = (Double,Double)
 -- in the compiler (Double,Double) and Point can be swapped around 
 --
-p1 :: (Double,Double)
-p1 = (1,2) 
+         p1 :: (Double,Double)
+         p1 = (1,2) 
 
-p2 :: Point 
-p2 = (3,4) 
+         p2 :: Point 
+         p2 = (3,4) 
 
 {-
 *CusDataTyp> :i p1
@@ -31,31 +31,42 @@ BUT THIS IS INCONSISTENT USE NEWTYPES INSTEAD
 
 -}
 
----- NEWTYPES ------
--- sort of key and value pairing 
---------Constructor-------------
----------------------Construction------
------------------------------------value
-newtype CustomerId = CustomerId Int 
+        ---- NEWTYPES ------
+        -- sort of key and value pairing 
+        --------Constructor-------------
+        ---------------------Construction------
+        -----------------------------------value
+         newtype CustomerId = CustomerId Int 
 
-customer = CustomerId 13 
+         customer = CustomerId 13 
 
-customerToInt (CustomerId i) = i 
+         customerToInt (CustomerId i) = i 
 
--- Constructor Names often match the Type Names. 
--- Newtypes make unique types from existing ones
--- This enforces semantic meaning through the type checker. 
-data Customer   = MakeCustomer 
-  { customerId    :: CustomerId 
-   , name         :: String 
-   , luckyNumber  :: Int 
-   } 
 
-alice :: Customer 
-alice = MakeCustomer 
-  { customerId    = CustomerId 13 
-  , name          = "Alice" 
-  , luckyNumber   = 42 
-  } 
- 
+
+
+
+    
+        -- Constructor Names often match the Type Names. 
+        -- Newtypes make unique types from existing ones
+        -- This enforces semantic meaning through the type checker. 
+
+         data Customer   = MakeCustomer 
+           { customerId    :: CustomerId 
+           , name         :: String 
+           , luckyNumber  :: Int 
+           } 
+
+         alice :: Customer 
+         alice = MakeCustomer 
+           { customerId    = CustomerId 13 
+           , name          = "Alice" 
+           , luckyNumber   = 42 
+           } 
+         
+
+
+
+
+
 
