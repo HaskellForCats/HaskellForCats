7914eab82261e1a3f4d116cc28312aa8d95a3e7c
evx001 <eviexeris@yahoo.com>
2014-02-03 20:58:18 -0500

    Records

--- Ch08/2014-0201-cusDataTypeRecords.hs
+++ Ch08/2014-0201-cusDataTypeRecords.hs
@@ -80,13 +80,39 @@ data Person = Person    { firstName :: String
 -- *CusDataTyp> :i age
 -- data Person = Person {..., age :: Float, ...}
 
-data Car = Car {company :: String, model :: String, year :: Int} deriving (Show) 
+-- data Car = Car {company :: String, model :: String, year :: Int} deriving (Show) 
+data Car a b c = Car { company :: a 
+                     , model   :: b 
+                     , year    :: c 
+                     } deriving (Show) 
+
+
+tellCar (Car {company = c, model = m, year = y}) = "This" ++ c ++ " " ++ m ++ "was made in " ++ show y 
 
 -- *CusDataTyp> Car {company="Ford", model="Mustang", year=1967}
 -- Car {company = "Ford", model = "Mustang", year = 1967}
+stang = Car {company="Ford", model="Mustang", year=1967} 
+-- *CusDataTyp> tellCar stang
+-- "ThisFord Mustangwas made in 1967"
 
+data Maybe a = Nothing | Just a 
 
+data Vector a = Vector a a a deriving (Show)  
   
+vplus :: (Num t) => Vector t -> Vector t -> Vector t  
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)  
+  
+vectMult :: (Num t) => Vector t -> t -> Vector t  
+(Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)  
+  
+scalarMult :: (Num t) => Vector t -> Vector t -> t  
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n  
+{-
+data Person = Person { firstName :: String  
+                     , lastName :: String  
+                     , age :: Int  
+                     } deriving (Eq)  
+-}
 -- Use record syntax when a constructor has several fields and it's not obvious which field is which. 
 
 {- 	ALGEBRAIC DATA TYPES  	-- 
