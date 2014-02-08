6a23e1ec7f136804d38d2cdb1182717cd2946b9f
evx001 <eviexeris@yahoo.com>
2014-02-08 15:45:23 -0500

    doBlock2

--- Ch08/IO/2014-0208IO.hs
+++ Ch08/IO/2014-0208IO.hs
@@ -1,5 +1,11 @@
 module IoFeb08 where 
-{- because Haskell functions are Pure 
+{- 
+: set lineBreak 
+: set spell 
+: set noSpell
+: syntax enable 
+
+because Haskell functions are Pure 
 	they cannot modify external state 
 	nor can they depend on external values 
 	they can't even write to the console directly 
@@ -19,7 +25,7 @@ putStrLn :: String -> IO () -- has type IO () which means it returns an IO Unit
 	-- an IO Unit is an IO Action.
 	-- it represents some code that could be run and interact with the outside world. 
 	-- this code could be run but doesn't get run without an IO Action. Thus you could call this
-Main is signifigant because IO doesn't happen without it. 
+Main is significant because IO doesn't happen without it. 
 	the runtime environment looks for main and if has an IO Action associated with it, that IO Action gets executed and side-effects are performed. 
 ---------- In this Program -----------
 
@@ -35,5 +41,44 @@ Linking main2 ...
 ➜  IO git:(master) ✗ ./main2
 Hi there!
 
+
+---------- IN THIS NEXT PROGRAM -----------
+main :: IO ()
+main = do
+        putStrLn "Welcome"
+        putStrLn "to Haskell IO"
+x = 3 -- is outside the do block and gets ignored.  
+
+
+➜  IO git:(master) ✗ ghc doBlock.hs    
+[1 of 1] Compiling Main             ( doBlock.hs, doBlock.o )
+Linking doBlock ...
+➜  IO git:(master) ✗ ./doBlock 
+Welcome
+to Haskell IO
+---------------------------------------------
 	 
 
+---------- IN THIS THIRD PROGRAM -----------
+helloWorld :: IO () 
+helloWorld = putStrLn "Hello World" 
+
+main :: IO () 
+main = do 
+	helloWorld 
+	helloWorld 
+	helloWorld 
+------------ WE COMPILE "doBlock2"  AND RUN IT --------------
+➜  IO git:(master) ✗ ghc doBlock2.hs 
+[1 of 1] Compiling Main             ( doBlock2.hs, doBlock2.o )
+Linking doBlock2 ...
+➜  IO git:(master) ✗ ./doBlock2     
+Hello World
+Hello World
+Hello World
+---------------------------------------------
+it is worth noting that putStrLn doesn't get run directly; it only sees Action in the main-DO-Block
+In other programming languages putStrLn would print immediately and not have to wait until it saw DO-Block Action
+
+ 
+---------- IN THIS FOURTH PROGRAM -----------
