a08937dc4ff59c35e64777f1e1518b446cb46086
evx001 <eviexeris@gmail.com>
2014-02-11 18:11:49 -0500

    <- binds

---
+++ Ch08/2014-0211emtyTuple.hs
@@ -0,0 +1,20 @@
+--  the empty tuple, () 
+--  also know as unit, is an IO Action which includes a return value inside it, 
+--  if no actual return value then the empty () is returned. 
+--  The empty tuple is a value of () and it also has a type of ().
+--  An I/O action will be performed when we give it a name of main and then run our program.
+--  the do syntax to glues together several I/O actions into one.
+main = do  
+    putStrLn "Hello, what's your name?"  
+    name <- getLine  
+    putStrLn ("Hi " ++ name ++ ", welcome to Haskell")
+
+-- By convention, we don't usually specify a type declaration for main. 
+-- main :: IO () 
+-- getLine :: IO String 
+-- name <- getLine  
+-- means perform the I/O action getLine  
+-- then bind its result value to name. 
+-- getLine has a type of IO String, so name will have a type of String. 
+-- the only way to access data inside the bind is to use the <- construct. 
+--
