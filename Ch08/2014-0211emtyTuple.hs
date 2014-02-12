408faaa58b79b78cc900b57a9ada1dddd84131d7
evx001 <eviexeris@gmail.com>
2014-02-11 19:20:12 -0500

    ...

--- Ch08/2014-0211emtyTuple.hs
+++ Ch08/2014-0211emtyTuple.hs
@@ -1,14 +1,16 @@
+import Control.Monad 
 --  the empty tuple, () 
 --  also know as unit, is an IO Action which includes a return value inside it, 
 --  if no actual return value then the empty () is returned. 
 --  The empty tuple is a value of () and it also has a type of ().
 --  An I/O action will be performed when we give it a name of main and then run our program.
 --  the do syntax to glues together several I/O actions into one.
+{-
 main = do  
     putStrLn "Hello, what's your name?"  
     name <- getLine  
     putStrLn ("Hi " ++ name ++ ", welcome to Haskell")
-
+-}
 -- By convention, we don't usually specify a type declaration for main. 
 -- main :: IO () 
 -- getLine :: IO String 
@@ -18,3 +20,15 @@ main = do
 -- getLine has a type of IO String, so name will have a type of String. 
 -- the only way to access data inside the bind is to use the <- construct. 
 --
+main = do   
+    line <- getLine  
+    if null line  
+        then return ()  
+        else do  
+            putStrLn $ reverseWords line  
+            main  
+  
+reverseWords :: String -> String  
+reverseWords = unwords . map reverse . words 
+
+
