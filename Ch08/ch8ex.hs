1927f0991079b17e34e5f74acbf861a945d14490
evx001 <eviexeris@yahoo.com>
2014-02-21 18:44:51 -0500

    parse to 11:21

--- Ch08/ch8ex.hs
+++ Ch08/ch8ex.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE NPlusKPatterns #-}
 module Ch8ex where 
 -- : set expandtab ts=4 ruler number spell
+-- enable syntax 
+-- 
 import Test.QuickCheck 
 -- import Data.List 
 import  qualified Data.Attoparsec.ByteString.Char8 as A -- for isDigit 
@@ -10,25 +12,54 @@ import Data.Char
 -- 6:07 -- 	-- PAPERS --
 ----------------------------------
 -----------------------------------------------------
---  Chapter 8 parser as special monad
+--  CHAPTER 8 PARSER AS SPECIAL CASE MONAD!
 -------------------------------------------------------
-parsers analyse text for syntactic structure 
-because a string is just a list of [Char] 
--------------------------------
-2 * 3 + 4  -- is --
-                        + 
-                       / \
-                      *   4
-                     / \
-                    2  3
+-- text tends to be unstructured data so we will take it in and parse it. 
+-- parsers analyse text for syntactic structure 
+-- because strings are just a  [[Char],[Char]] etc.  
+-- we can add more structure a tree. 
+-- 
+-- -------------------------------
+-- 2 * 3 + 4  -- is --
+--                         + 
+--                        / \
+--                       *   4
+--                      / \
+--                     2  3
+-- 
+-- 
+-- Because of the structure of our tree, addition is further away than multiplication; starting from the bottom and working our way up; the multiplication of 2*3 is bound, tighter and closer than is addition.  
+-- This way our tree conforms to the order of operations. Just like in real math. 
+-- 
+-- Our parser will take strings in and make data trees that have a hierarchal structure to them.   
+-- COOLNESS! I think.
+-- 
 
+-- NOTE! PREPROCESSING IS EVERYWHERE 
+-- -------------------------------------------------
+-- ghc 		parses 		haskell src code
+-- ------------------------------------------------
+-- linux/bash 	parses 		shell scripts 
+-- ------------------------------------------------
+-- firefox		parses 		html
+-- ----------------------------------------------- 
+-- 
+-- html is considered hard to parse
+-- all progs are parsing in some way 
+-- and because we are a using a functional language, no surprise, our parser will be a function.
+-- --------------------------------------------
+-- Parser is a function that takes Strings and returns Trees.
+{-   
+type Parser = String -> Tree
+	  ______________
+	  |		|
+String--->|		|---------> Tree
+	  |_____________|
 
-Because of the structure of our tree, addition is further away than multiplication; starting from the bottom and working way up, shows this tree conforming to the order of operations.  
-
-COOLNESS! I think.
+alas this is not very combinatory! 
 
 -------------------------------            
-
+-} 
 
 
 
