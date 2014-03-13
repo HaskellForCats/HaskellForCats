1bb91e9d6fa2dc3680e6b76c660bcdb2b33b2c8e
evx001 <eviexeris@gmail.com>
2014-03-12 22:35:28 -0400

    IO actions

--- ch09/2014-0309.hs
+++ ch09/2014-0309.hs
@@ -17,3 +17,24 @@ module Ch09 where
 
 -- we distinguish actions -- which have side effects -- from functions that don't.  
 -- we can use types to distinguish the two 
+-- because one has an action associated and the other doesn't 
+--
+-- IO () -- returns the identity element which can be used to tag actions for entry into our pure functional universe. 
+--
+-- type IO = World -> World 
+type IO a = World -> (a , World)
+
+-- expressions of type IO are called actions 
+--
+-- IO () are just side effecting, because they don't return a result.  
+-- The empty tuple is an empty tuple. 
+--
+-- Basic actions in Haskell 
+-- 	getChar :: IO Char 
+-- 	putChar :: Char -> IO ()
+-- 	return 	:: a -> IO a 
+-- 	return v = \world -> (v, world)
+--
+-- 	return offers a BRIDGE between non-side-effecting functions and the impure universe side-effecting actions  
+-- 	this is a small subset of functions, as most functions in Haskell effect nothing.
+-- 	Impurity is a contamination because once side-effects are introduced there can beno going back.
