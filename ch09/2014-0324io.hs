db580868482eb59f355f95adf8453b9fbfcb6ede
evx001 <eviexeris@gmail.com>
2014-03-29 05:26:16 -0400

    more

--- ch09/2014-0324io.hs
+++ ch09/2014-0324io.hs
@@ -1,6 +1,6 @@
 module Ch9io where
-
- 
+import Prelude hiding (getLine,putStr,putStrLn) 
+import System.IO hiding   (getLine,putStr,putStrLn)
 -- (>>=) :: Monad m => m a -> (a -> m b) -> m b 
 -- can be seen this way 
 -- (>>=) :: IO a -> (a -> IO b) -> IO b 
@@ -17,10 +17,43 @@ module Ch9io where
 --      }
 
 -- this can be seen as 
-
-getChar >>= \x -> 
-getChar >>= \_ -> 
-getChar >>= \y -> 
-     return (x,y) 
+-- 
+-- getChar >>= \x -> 
+-- getChar >>= \_ -> 
+-- getChar >>= \y -> 
+--      return (x,y) 
 
 -- getChar >>= \x -> \_ -> \y -> return (x,y)
+--
+-- ---18:00 -- post C# -------------------------------
+-- -- ----------------Drived-Primatives------------------
+-- getLine :: IO String 
+getLine = do x <- getChar 
+             if x == '\n' then 
+                  return [] 
+                else 
+                    do xs <- getLine 
+                       return (x:xs) 
+--   
+putStr 		:: String -> IO ()
+putStr [] 	= return () 
+putStr (x:xs) 	= do 	putChar x 
+			putChar x -- with just x it compiles but wont compile  
+
+putStrLn 	:: String -> IO () 
+putStrLn xs 	= do 	putStr xs 
+			putChar '\n' 
+
+-- Erik rewrites the above as:
+-- foldr (+) (return ()) 
+-- where
+-- x(+)p = putChar x >>= \_ -> P 
+
+strlen :: IO () 
+strlen = do 	putStr "Enter a string: " 
+		xs <- getLine 
+		putStr "The string has  " 
+		putStr (show (length xs)) 
+		putStrLn " characters" 
+
+ 
