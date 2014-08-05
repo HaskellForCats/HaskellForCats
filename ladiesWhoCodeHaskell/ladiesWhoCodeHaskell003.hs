ea8e8743e16e423bb22988850c4e1774ebda265f
evx001 <eviexeris@gmail.com>
2014-08-04 23:32:42 -0400

    003 awaits content

--- ladiesWhoCodeHaskell/ladiesWhoCodeHaskell003.hs
+++ ladiesWhoCodeHaskell/ladiesWhoCodeHaskell003.hs
@@ -3,21 +3,5 @@ module Lwch003 where
 -- :retab 
 -- :set expandtab ts=2 linebreak  
 -- :set +s 
--- let fn003 =  fn001 / fn000   
 
-fn000 =  (6 * (7 + 5 /2)^2) + 2
-fn001 = (+) (6 * (7 + 5 /2)^2) (2 * (product(take 5[1..])))
-
--------------------
--- fn003 see above
-------------------
-
-fn004 x y z  = x + y ^ z * w  
-              where 
-              w = sum(head[101..201] ++tail[1..100]) 
-              a = [] 
-
-------------------------------
--- :i fn004 
-----------------------------               
 
