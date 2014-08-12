8249a9cb57c606f53587eda3453a47ef65d3698b
evx001 <eviexeris@gmail.com>
2014-08-12 14:28:50 -0400

    vim extras

--- ladiesWhoCodeHaskell/ladiesWhoCodeHaskell005.hs
+++ ladiesWhoCodeHaskell/ladiesWhoCodeHaskell005.hs
@@ -1,6 +1,9 @@
 module Lwch005 where 
 -- : retab 
 -- : set +s 
+-- [count]ge  
+-- [count](
+-- [count]{
 --  set expandtab ts=4 ruler number spell linebreak
 ----------------------------
 -- Higher Order Functions 
