73769778ea4436452d4b72644c737dded1db6c35
evx001 <eviexeris@gmail.com>
2014-08-13 07:20:30 -0400

    vim commands

---
+++ ladiesWhoCodeHaskell/ladiesWhoCodeHaskell007.hs
@@ -0,0 +1,10 @@
+module Lwch006 where 
+-- : retab 
+-- : set +s 
+-- [count]ge  
+-- [count](
+-- [count]{
+--  set expandtab ts=4 ruler number spell linebreak
+
+main :: IO ()
+main = putStrLn "Hello World" 
