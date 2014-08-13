7268b7b8c9271cd861d5bc0342a843e88ea1efd7
evx001 <eviexeris@gmail.com>
2014-08-13 07:21:35 -0400

    cutting extra turing

---
+++ ladiesWhoCodeHaskell/ladiesWhoCodeHaskell001a.hs
@@ -0,0 +1,15 @@
+module Lwch001 where
+-- : retab 
+--: set expandtab ts=4 ruler number spell linebreak
+{-
+			lego turing machine ... 2+2
+			http://vimeo.com/44202270
+
+			film strip version of Turing Machine
+			https://www.youtube.com/watch?v=E3keLeMwfHY
+
+			Church and Turing @ prinston 
+			http://www.princeton.edu/main/news/archive/S33/59/04A71/index.xml?section=featured&path=/main/news/archive/S33/59/04A71/index.xml&next=1
+-}
+
+
