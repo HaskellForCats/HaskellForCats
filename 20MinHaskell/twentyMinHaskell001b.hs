0215dec17e11ddf364c89f3646a529d123e80330
evx001 <eviexeris@gmail.com>
2014-08-22 09:20:36 -0400

    001b

---
+++ 20MinHaskell/twentyMinHaskell001b.hs
@@ -0,0 +1,14 @@
+module TwntyMn1b where 
+-- : retab 
+-- : set expandtab ts=4 ruler number spell linebreak
+
+
+sqr x = x*x
+cube x = x*x*x
+
+-- map sqr [1..1001]
+
+-- *TwntyMn1b>  :t map cube [1..11]
+-- map cube [1..11] :: (Enum b, Num b) => [b]
+
+
