4b559adade42d1f94f8b6041df97c0de66339c69
evx001 <eviexeris@gmail.com>
2014-09-14 10:21:45 -0400

    modified

--- factorial.hs
+++ factorial.hs
@@ -1,4 +1,12 @@
 module Factorial where
-    factorial :: Double -> Double
-    factorial 0 = 1
-    factorial x = x * factorial (x - 1)
+
+factorial :: Double -> Double
+factorial 0 = 1
+factorial x = x * factorial (x - 1)
+
+factorial' n | n < 2 = 1
+factorial' n = n * factorial' (n - 1)
+
+factorial'' n = foldl1 (*) [1..n]
+
+
