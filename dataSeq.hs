001b964ad430332abf66f29037193a3fac4951ac
evx001 <eviexeris@gmail.com>
2015-01-26 03:28:11 -0500

    -XViewPatterns

--- dataSeq.hs
+++ dataSeq.hs
@@ -5,4 +5,11 @@ import Data.Sequence
 mySeq = empty 
 my2Seq = 2 <| mySeq
 my3Seq = empty  
+{-
+*DataSeq> 1 <| (my2Seq ><  (my3Seq |>  3))
+fromList [1,2,3]
+-- my2Seq ><  my3Seq
+-- my2Seq ><  (my3Seq <| 2)
 
+:set -XViewPatterns
+-}
