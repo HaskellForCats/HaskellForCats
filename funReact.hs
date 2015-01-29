7c039d468ba326343a9429c9e27e4a7fdd7b1726
evx001 <eviexeris@gmail.com>
2015-01-29 00:27:05 -0500

    functional Reactive at last

---
+++ funReact.hs
@@ -0,0 +1,35 @@
+module FunReact where 
+
+import Control.Monad 
+import Data.Maybe
+import Text.Read (readMaybe)
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
+
+module Main where
+
+main =  startGUI defaultConfig setup
+
+setup window = do 
+	(return window) # set title "Addtion"
+
+	input1 <- UI.input
+	input2 <- UI.input
+	result <- UI.span 
+
+	getBody window #+ 
+		[ element input1
+		, string " + "
+		, element input2
+		, string " = "
+		, element result ]
+	bInput1 <- bValue input1
+	bInput2 <- bValue input2
+
+	let bInput1Num = (liftA readMaybe) bInput1
+		bInput2Num = (liftA readMaybe) bInput2
+		bSum * (liftA2 (liftM2 (+))) bInput1Num bInput2Num
+		bResult = (liftA showMaybe) bSum
+
+	element result # sink text bResult
+	return ()
