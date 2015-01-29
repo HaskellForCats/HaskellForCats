85c2375303bac243ec27834e7e3856cb61bea66c
evx001 <eviexeris@gmail.com>
2015-01-29 06:20:20 -0500

    showMaybe instance

--- funReact.hs
+++ funReact.hs
@@ -1,4 +1,4 @@
-module FunReact where 
+-- module FunReact where 
 
 import Control.Monad 
 import Data.Maybe
@@ -6,7 +6,7 @@ import Text.Read (readMaybe)
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 
-module Main where
+
 
 main =  startGUI defaultConfig setup
 
@@ -26,10 +26,12 @@ setup window = do
 	bInput1 <- bValue input1
 	bInput2 <- bValue input2
 
-	let bInput1Num = (liftA readMaybe) bInput1
+	let 	bInput1Num = (liftA readMaybe) bInput1
 		bInput2Num = (liftA readMaybe) bInput2
-		bSum * (liftA2 (liftM2 (+))) bInput1Num bInput2Num
+		bSum = (liftA2 (liftM2 (+))) bInput1Num bInput2Num
 		bResult = (liftA showMaybe) bSum
 
 	element result # sink text bResult
 	return ()
+
+-- instance showMaybe :: (Show a) => Show (Maybe a)
