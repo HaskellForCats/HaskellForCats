9a1a310b8cc8a62d46a74452b4c81c497f51279e
evx001 <eviexeris@gmail.com>
2015-01-29 06:45:11 -0500

    ...

--- funReact.hs
+++ funReact.hs
@@ -11,27 +11,27 @@ import Graphics.UI.Threepenny.Core
 main =  startGUI defaultConfig setup
 
 setup window = do 
-	(return window) # set title "Addtion"
+        (return window) # set title "Addtion"
 
-	input1 <- UI.input
-	input2 <- UI.input
-	result <- UI.span 
+        input1 <- UI.input
+        input2 <- UI.input
+        result <- UI.span 
 
-	getBody window #+ 
-		[ element input1
-		, string " + "
-		, element input2
-		, string " = "
-		, element result ]
-	bInput1 <- bValue input1
-	bInput2 <- bValue input2
+        getBody window #+ 
+                [ element input1
+                , string " + "
+                , element input2
+                , string " = "
+                , element result ]
+        bInput1 <- bValue input1
+        bInput2 <- bValue input2
 
-	let 	bInput1Num = (liftA readMaybe) bInput1
-		bInput2Num = (liftA readMaybe) bInput2
-		bSum = (liftA2 (liftM2 (+))) bInput1Num bInput2Num
-		bResult = (liftA showMaybe) bSum
+        let     bInput1Num = (liftA readMaybe) bInput1
+                bInput2Num = (liftA readMaybe) bInput2
+                bSum = (liftA2 (liftM2 (+))) bInput1Num bInput2Num
+                bResult = (liftA showMaybe) bSum
 
-	element result # sink text bResult
-	return ()
+        element result # sink text bResult
+        return ()
 
 -- instance showMaybe :: (Show a) => Show (Maybe a)
