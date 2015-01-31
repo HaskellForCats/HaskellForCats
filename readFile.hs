e4ed5684c039c858813bb6766aba3a0a9c1a355b
evx001 <eviexeris@gmail.com>
2015-01-31 15:56:12 -0500

    read file 2nd attempt

--- readFile.hs
+++ readFile.hs
@@ -2,8 +2,8 @@ module ReadFile where
 
 
 main = do 
-	input <- readFile 20140819oopAfterHask
-	
-	--countWords input = map 
-	--(lenght.words) (lines input) 
+	input <- readFile "input.txt" 
+
+	countWords input = map 
+	(lenght.words) (lines input) 
 
