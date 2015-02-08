a0bbb5a41e94dd1c69f01502a21edddbc7a98951
evx001 <eviexeris@gmail.com>
2015-02-07 22:32:15 -0500

    the csv version

---
+++ EstTaxQrt/estTax1stQrt.hs
@@ -0,0 +1,84 @@
+module  EstTax1stQrt where
+-- (<*>) :: f (a -> b) -> f a -> f b 
+        -- Sequential application.
+-- (<$>) :: Functor f => (a -> b) -> f a -> f b 
+        -- An infix synonym for fmap.
+
+import Text.CSV 
+import Data.String
+
+data MonthExpense  = MonthExpense  
+                     { date     :: String
+                     , amount   :: String
+                     , detail   :: String
+                     , debit    :: String
+                     } 
+
+main = do 
+   let fileName = "x20150108.csv" 
+   input <- readFile fileName
+   let csv = parseCSV fileName input
+{-      either handleError doWork  csv -}
+   print  csv  
+handleError csv = putStrLn "not a CSV"
+
+-- "\"2015-01-01\",\"$3.24\",\"APL* ITUNES.COM/BILL   866-712-7753  CA\",\"2\"\n\
+
+{- 
+doWork csv = (print.findOldest.tail) (filter (\x -> length x == 2) csv)
+
+-- Finds oldest person.
+findOldest :: [Record] -> Record
+findOldest [] = []
+findOldest items = foldl1 (\a x -> if age x > age a then x else a) items
+
+age [a,b] = toInt b
+
+toInt :: String -> Int                               
+toInt = read
+
+--------------------------------------------
+-- constants for EstTax
+--------------------------------------------
+nycOglig        = 0.03534
+albanyOblig     = 0.059
+fedObligat      = 0.3 
+
+-- imported expenses into a list 
+expen = [dec02,dec26,nov09,oct07,sept07]
+
+-- totaling the list
+sumExpen = sum expen 
+---------------------------------------------
+-- totaling earned and subtracting expenses
+earned4Qt = (sum [earnedSep,earnedOct,earnedNov,earnedDec,prevQt]) - sumExpen
+                where  
+                earnedSep = 0 
+                earnedOct = 9000
+                earnedNov = 0 
+                earnedDec = 0 
+                prevQt= (0 + prev)
+                        where  
+                        prev = 0  -- anything not otherwise included above
+
+---------------------------------
+-- formulary for arriving at the Fed, State, and local plus the total quaterly
+taxFed = earned4Qt * fedObligat
+taxAlbany = ( earned4Qt*albanyOblig)
+taxNYC = ( earned4Qt*nycOglig)
+taxTotalObligado = sum[taxFed, taxAlbany, taxNYC] 
+---------------------------------
+-}
+------- result -----------------
+{- 
+*EstTax4Qrt> taxFed
+2028.474
+*EstTax4Qrt> taxAlbany
+398.93321999999995
+*EstTax4Qrt> taxNYC
+238.95423720000002
+*EstTax4Qrt> taxTotalObligado
+2666.3614572
+-}
+
+
