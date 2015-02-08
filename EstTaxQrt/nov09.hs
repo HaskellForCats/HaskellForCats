cc996d75a378affc3d558ece5576308c832a1394
evx001 <eviexeris@gmail.com>
2015-02-07 22:31:12 -0500

    the data

---
+++ EstTaxQrt/nov09.hs
@@ -0,0 +1,54 @@
+module Nov09 where
+nov09 = sum [
+		29.00 ,
+		2.17 , 	
+		18.00 ,
+		10.00 ,
+		7.00 , 
+		5.80 , 
+		10.00 ,
+		9.79 , 
+		10.00 ,
+		18.96 ,
+		5.00 , 
+		18.50 ,
+		7.00 , 
+		25.00 ,
+		10.00 ,
+		1.08 , 
+		19.05 ,
+		7.20 , 
+		16.46 ,
+		21.76 ,
+		5.99  
+	]
+
+
+
+
+
+
+
+{-
+	 2014-10-11 ,  29.00 , PLURALSIGHT LLC        HINGHAM       MA , 2 ,
+	 2014-10-12 ,  2.17 , APL* ITUNES.COM/BILL   866-712-7753  CA , 2 ,
+	 2014-10-12 ,  18.00 , LIRRNYTICKETS      QPS JAMAICA       NY , 2 ,
+	 2014-10-13 ,  10.00 , MTA MVM*34TH STREETQPS NEW YORK      NY , 2 ,
+	 2014-10-15 ,  7.00 , AMAZON MKTPLACE PMTS   AMZN.COM/BILL WA , 2 ,
+	 2014-10-15 ,  5.80 , AMAZON MKTPLACE PMTS   AMZN.COM/BILL WA , 2 ,
+	 2014-10-15 ,  10.00 , MTA MVM*HOUSTON STRQPS NEW YORK      NY , 2 ,
+	 2014-10-17 ,  9.79 , Amazon.com             AMZN.COM/BILL WA , 2 ,
+	 2014-10-18 ,  10.00 , MTA MVM*14TH STREETQPS NEW YORK      NY , 2 ,
+	 2014-10-20 ,  18.96 , Amazon.com             AMZN.COM/BILL WA , 2 ,
+	 2014-10-23 ,  5.00 , PAYPAL *RH88           4029357733    CA , 2 ,
+	 2014-10-23 ,  18.50 , MTA MVM*CHAMBERS STQPS NEW YORK      NY , 2 ,
+	 2014-10-24 ,  7.00 , GH *GITHUB.COM    WOHA 4157354488    CA , 2 ,
+	 2014-10-26 ,  25.00 , WWW.TEAMTREEHOUSE.COM  8009282130    OR , 2 ,
+	 2014-10-28 ,  10.00 , MTA MVM*CHAMBERS STQPS NEW YORK      NY , 2 ,
+	 2014-11-01 ,  1.08 , APL* ITUNES.COM/BILL   866-712-7753  CA , 2 ,
+	 2014-11-03 ,  19.05 , MTA MVM*CHAMBERS STQPS NEW YORK      NY , 2 ,
+	 2014-11-04 ,  7.20 , NYC TAXI 3L21          LONG ISLAND C NY , 2 ,
+	 2014-11-04 ,  16.46 , TERRYS DELI            NEW YORK      NY , 2 ,
+	 2014-11-07 ,  21.76 , Safari Books Online    7078274118    MA , 2 ,
+	 2014-11-09 ,  5.99 , Amazon Services-Kindle 866-321-8851  WA , 2 
+-}
