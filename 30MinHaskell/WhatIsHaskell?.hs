8b8dd5ef4648ba0eb05801135d45af487b868b92
evx001 <eviexeris@gmail.com>
2015-08-19 23:52:01 -0400

    Outline

---
+++ 30MinHaskell/WhatIsHaskell?.hs
@@ -0,0 +1,24 @@
+{-
+What is Haskell? 
+
+Haskell is a General purpose programming language.
+
+Haskell is purely functional.
+
+Haskell is strictly statically typed. 
+
+Haskell is lazy by default. 
+
+Haskell has Type inference.
+
+This Trinity of the weird features, make it about as far away from the C paradigm as you can currently get and still expect get significant work done. 
+
+Experience with Lisp or Clojure reduce the the exoticism, as would experience with scheme or F sharp or ocaml. But even those don't quite go as far away from familiar Territory as Haskell. 
+
+Most commercial languages are bound to their C heritage. They can head in the direction of Haskell but never quite get there because of the compromises that must be made in order to satisfy their C Origins. 
+
+-}
+
+head000 [] = 0
+head000 xs  = head xs
+ 
